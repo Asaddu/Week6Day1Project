9f8d7d471f1b82b005156e65719aee4423ddbf3d
Mikel Robbins <mikel@asaddu.com>
2023-04-09 22:23:03 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+storeItems.forEach(function(item) {
+    if(item.inStock) {
+        $('#itemTable').append(`<tr><td>$${item.price}</td><td>${item.name}</td><td>${item.details}</td></tr>`)
+    }
+});
+
+$("#toggleDarkMode").on("click", function () {
+    $("body").toggleClass("dark-mode");
+    $("tr").toggleClass("dark-mode");
+    $("button").toggleClass("dark-mode");
+});
